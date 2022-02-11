12803449894b871316bc85f53628956970455317
gemmaellis16 <gemmaellis16@hotmail.com>
2022-02-11 12:49:34 +0000

    add game-grid js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,97 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  //card options
+
+  const deckOfCards = [
+    {
+      name: 'chicken',
+      img: 'assets/images/chicken.png'
+  
+  }
+  {
+    name: 'chicken',
+    img: 'assets/images/chicken.png'
+
+}
+{
+  name: 'cow',
+  img: 'assets/images/cow.png'
+
+}
+{
+  name: 'cow',
+  img: 'assets/images/cow.png'
+
+}
+{
+  name: 'dog',
+  img: 'assets/images/dog.png'
+
+}
+{
+  name: 'dog',
+  img: 'assets/images/dog.png'
+
+}
+{
+  name: 'goat',
+  img: 'assets/images/goat.png'
+
+}
+{
+  name: 'goat',
+  img: 'assets/images/goat.png'
+
+}
+{
+  name: 'goose',
+  img: 'assets/images/goose.png'
+
+}
+{
+  name: 'goose',
+  img: 'assets/images/goose.png'
+
+}
+{
+  name: 'horse',
+  img: 'assets/images/horse.png'
+
+}
+{
+  name: 'horse',
+  img: 'assets/images/horse.png'
+
+}
+{
+  name: 'pig',
+  img: 'assets/images/pig.png'
+
+}
+{
+  name: 'pig',
+  img: 'assets/images/pig.png'
+
+}
+{
+  name: 'sheep',
+  img: 'assets/images/sheep.png'
+
+}
+{
+  name: 'sheep',
+  img: 'assets/images/sheep.png'
+
+}
+]
+
+const gameGrid = document.querySelector('.game-grid')
+
+//game-grid
+ for (let card of deckOfCards) {
+   const nameRef = document.createElement("img");
+   gridRef.insertAdjacentHTML("beforehand", '${card.name}' )
+ }
 
 
 //Popovers with username /level of difficulty
@@ -62,10 +154,7 @@ function label(val) {
 
 
   //reset timer
-  let timer = document.querySelector(".timer");
-  timer.innerHTML = "0 mins 0 secs";
-  clearInterval(interval);
-}
+  
 
 //Audio for cards
 
